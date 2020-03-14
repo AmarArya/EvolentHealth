7b7835f0530e13b86e39dda48f26ec28e15e3f90
AmarArya <arya.amar@gmail.com>
2020-03-14 16:52:56 +0530

    missed logger file

---
+++ EvolentHealth.CrossCutting.Logging/LogService/Logger.cs
@@ -0,0 +1,25 @@
+﻿using EvolentHealth.CrossCutting.Logging.LogInterface;
+using log4net;
+
+namespace EvolentHealth.CrossCutting.Logging.LogService
+{
+	public class Logger : ILogger
+	{
+		private readonly ILog _log = LogManager.GetLogger("Log");
+
+		static Logger()
+		{
+			log4net.Config.XmlConfigurator.Configure();
+		}
+
+		public void LogInfo(string message, params object[] args)
+		{
+			_log.Info(string.Format(message, args));
+		}
+
+		public void LogError(string message, params object[] args)
+		{
+			_log.Error(string.Format(message, args));
+		}
+	}
+}
